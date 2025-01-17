1fe5d2bc10a0c40f7a09c1df24cdd2cb28e8c86d
Jaime Rubio <jaime.rubio.lopez@ces-vegamedia.es>
2025-01-17 09:50:32 +0100

    Archivo js variables
    cambiamos el archivo js

---
+++ JS/PrimerEjemplo/02-variables.js
@@ -0,0 +1,24 @@
+//VARIABLES
+// Una variable es un contenedor de información
+
+//Variables de tipo String
+var pais="España";
+var continente="Europa";
+
+console.log(pais);
+console.log(pais,continente);
+
+pais="México";
+continente="Latinoamerica";
+
+console.log(pais);
+console.log(pais,continente);
+
+//Variables de tipo entero
+var antiguedad=2019;
+console.log(antiguedad);
+console.log(pais,continente,antiguedad);
+
+//Concadenar variables
+var pais_y_continente=pais+' '+continente;
+alert(pais_y_continente);
